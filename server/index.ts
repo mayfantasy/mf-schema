831373137965ae3d47ed75a855288b4836a8d09f
hansknowyou <yuelun123321@hotmail.com>
2019-12-15 00:20:16 -0500

    Added deployment scripts

--- server/index.ts
+++ server/index.ts
@@ -28,7 +28,7 @@ import {
   deleteObjectByIdRoute
 } from './routes/object.route'
 
-const port = parseInt(process.env.PORT || '3000', 10)
+const port = parseInt(process.env.PORT || '8080', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
